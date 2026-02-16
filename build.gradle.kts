01f8e465b17aa2d80f3fab587360240ac018e314
dg <114802062+WdgW@users.noreply.github.com>
2026-02-17 02:09:51 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,9 +47,7 @@ allprojects {
         maven {
             url = uri("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository")
         }
-        maven {
-            url = uri("https://www.jitpack.io")
-        }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
